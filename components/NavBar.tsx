bacbbd106209ef2cc2d849f064123f2eaae3ab55
Jkhall81 <jason.kei.hall@gmail.com>
2024-04-11 10:48:55 -0700

    almost done with navbar, just need to add modals / all on click functionality to navbar elements.  visually its done.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,8 +2,12 @@ import { FaReddit } from "react-icons/fa";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CiSearch } from "react-icons/ci";
 import { BsThreeDots } from "react-icons/bs";
+import { MdOutlineQrCode2 } from "react-icons/md";
+
 import { Separator } from "@/components/ui/separator";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 export const NavBar = () => {
   return (
@@ -15,14 +19,29 @@ export const NavBar = () => {
             size={35}
             className="ml-5 hover:cursor-pointer lg:hidden"
           />
-          <FaReddit color="red" size={35} className="mx-4" />
-          <span className="text-3xl font-bold text-white">Teddit</span>
+          <Link className="flex items-center" href="/">
+            <FaReddit color="red" size={35} className="mx-4" />
+            <span className="text-3xl font-bold text-white">Teddit</span>
+          </Link>
         </div>
         <div>
           <CiSearch className="absolute top-4 ml-2" color="white" size={30} />
           <Input placeholder="Search Teddit" />
         </div>
-        <div></div>
+        <div className="flex items-center">
+          <Button className="mr-2 hidden lg:flex" variant="app" size="sm">
+            <MdOutlineQrCode2 className="mr-2" color="white" size={25} />
+            <span className="text-white">Get app</span>
+          </Button>
+          <Button className="mr-5" variant="destructive" size="sm">
+            Log in
+          </Button>
+          <BsThreeDots
+            color="white"
+            className="mr-5 hover:cursor-pointer"
+            size={25}
+          />
+        </div>
       </nav>
       <Separator orientation="horizontal" className="" />
     </div>
