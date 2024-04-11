ec837bebc18cf5940067e796b285bb2f6a3543ae
Jkhall81 <jason.kei.hall@gmail.com>
2024-04-11 11:22:29 -0700

    start on the side bar, got it to pop out when the left hamburger menu disappears, and it disapepars when the hamburger menu appears.  put a right border on it.  now time to style the interior.

---
+++ components/SideBar.tsx
@@ -0,0 +1,7 @@
+export const SideBar = () => {
+  return (
+    <aside className="left-0 top-0 z-40 hidden h-screen w-[280px] border border-r-white lg:flex">
+      <span className="text-white">sidebar</span>
+    </aside>
+  );
+};
